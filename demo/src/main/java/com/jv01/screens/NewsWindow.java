b1dfa3d1020ea9ff743ebf8acd84d720f4026aad
lucas <lucas.menot@viacesi.fr>
2023-11-08 12:23:29 +0100

    Random events are now displayed in morning news !

--- demo/src/main/java/com/jv01/screens/NewsWindow.java
+++ demo/src/main/java/com/jv01/screens/NewsWindow.java
@@ -2,6 +2,7 @@ package com.jv01.screens;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 import com.jv01.fonctionals.Save;
 import com.jv01.generations.Game;
@@ -13,6 +14,7 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 
 import java.util.Map;
+import java.util.Random;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -22,30 +24,76 @@ import java.awt.event.ActionListener;
 
 public class NewsWindow extends JFrame {
 
+    private Save save = new Save();
+    private Document randomEventsDocument;
+    private Element randomEventChoosed;
+    private Random random = new Random();
+
     public NewsWindow(String gameName) {
         setTitle("Morning News");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        displayMorningNews();
+        randomEventsDocument = GetRandomEventsDoc(gameName);
+        randomEventChoosed = getRandomEvent(randomEventsDocument);
+        displayMorningNews(randomEventChoosed);
         generateRandomEvent();
     }
 
-    private void displayMorningNews() {
-        // Ajoutez ici le contenu du journal du matin, par exemple des articles et des images.
-        // Vous pouvez utiliser des composants Swing comme JLabel, JTextArea, etc.
-        // Exemple :
-        JTextArea newsText = new JTextArea("Voici les nouvelles du matin :\n\n"
-                + "1. Titre de l'article 1\n"
-                + "   Contenu de l'article 1...\n\n"
-                + "2. Titre de l'article 2\n"
-                + "   Contenu de l'article 2...\n\n");
-        newsText.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(newsText);
+    private void displayMorningNews(Element randomEvent) {
+        if (randomEvent != null) {
+            String eventName = save.getChildFromElement(randomEvent, "name");
+            String eventText = save.getChildFromElement(randomEvent, "text");
+            
+            JTextArea newsText = new JTextArea("Voici les nouvelles du matin :\n\n"
+                + "1."+ eventName +"\n"
+                + "   "+ eventText +"\n\n");
+            newsText.setEditable(false);
+            JScrollPane scrollPane = new JScrollPane(newsText);
 
-        add(scrollPane, BorderLayout.CENTER);
+            add(scrollPane, BorderLayout.CENTER);
+
+            System.out.println("Random Event Name: " + eventName);
+            System.out.println("Random Event Text: " + eventText);
+        } 
+        else 
+        {
+            JTextArea newsText = new JTextArea("Voici les nouvelles du matin :\n\n"
+                + "1.CA NA PAS MARCHE HAHAHAHAHAHAH\n"
+                + "  nullosse...\n\n"
+                + "2. nulnulnul\n"
+                + "   supernul\n\n");
+            newsText.setEditable(false);
+            JScrollPane scrollPane = new JScrollPane(newsText);
+
+            add(scrollPane, BorderLayout.CENTER);
+        }
+    }
+
+    private Document GetRandomEventsDoc(String gameName)
+    {
+        return randomEventsDocument = save.getDocumentXml(gameName, "randomEvents");
+    }
+
+    private Element getRandomEvent(Document doc) {
+        // Get the list of randomEvent elements
+        NodeList randomEventNodes = doc.getElementsByTagName("randomEvent");
+        
+        // Generate a random number within the range of available IDs
+        int randomID = random.nextInt(randomEventNodes.getLength()) + 1;
+        
+        // Iterate through the randomEvent elements and find the one with the matching ID
+        for (int i = 0; i < randomEventNodes.getLength(); i++) {
+            Element randomEventElement = (Element) randomEventNodes.item(i);
+            int eventID = Integer.parseInt(randomEventElement.getAttribute("id"));
+            if (eventID == randomID) {
+                return randomEventElement; // Return the selected random event element
+            }
+        }
+        
+        return null; // Return null if no matching event is found
     }
 
     private void generateRandomEvent() {
-        // Faire des trucs sympa genre MANIFESTATION
+        System.out.println("Aie aie aie !");
     }
 }
